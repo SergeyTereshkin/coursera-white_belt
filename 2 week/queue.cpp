adfb95ab85b2607f98c86dee943f247d8508658f
SergeyTereshkin <63157212+SergeyTereshkin@users.noreply.github.com>
2020-11-17 18:48:41 +0300

    Update queue.cpp

--- 2 week/queue.cpp
+++ 2 week/queue.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <string>
 
+using namespace std;
+
+int main() {
+    int Q;
+    cin >> Q;
+    vector<string> commands(Q + 1);
+    vector<bool> queue;
+    for (string&s : commands) {
+        getline(cin, s);
+    }
+
+    for (int i = 1; i < commands.size(); i++) {
+        size_t pos = commands[i].find(" ");
+        string command1 = commands[i].substr(0, pos);
+        if (command1 != "WORRY_COUNT") {
+            int number = stoi(commands[i].substr(pos + 1));
+            if (command1 == "WORRY") {
+                queue[number] = true;
+            } else if (command1 == "QUIET") {
+                    queue[number] = false;
+            } else if (command1 == "COME") {
+                queue.resize(queue.size() + number);
+            }
+        }
+        else if (command1 == "WORRY_COUNT")
+        {
+            int counter = 0;
+            for (auto s : queue)
+            {
+                if (s)
+                {
+                    counter++;
+                }
+            }
+            cout << counter << endl;
+        }
+    }
+    return 0;
+}
